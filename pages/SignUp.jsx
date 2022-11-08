40bfe1ae88ae3e8c49623a43a5c6b863bffdd320
O.B Sofiane <104198135+Agellid03@users.noreply.github.com>
2022-11-08 11:59:46 +0100

    Add files via upload

---
+++ pages/SignUp.jsx
@@ -0,0 +1,71 @@
+import React from "react";
+import Navbar from "../Navbar";
+
+function SignUp() {
+  return (
+    <div class="container-md ">
+      <div class="text-center">
+        <Navbar />
+      </div>
+      <h2>Inscrivez-vous ! </h2>
+      <div class="row  justify-content-center">
+        <div class="col-lg-4 mt-5">
+          <h2 class="row justify-content-center">Inscrivez-vous!</h2>
+          <label for="Nom" class="form-label" type="text">
+            Votre prénom
+          </label>
+          <input
+            type="text"
+            class="form-control form-control "
+            placeholder="EX: Sofiane,Dorian.."
+          />
+
+          <label for="Nom" class="form-label" type="text">
+            Votre Nom
+          </label>
+          <input type="text" class="form-control" placeholder="...." />
+
+          <label for="email" class="form-label mt-3">
+            Veuillez entrez votre email
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="EX: exemple@exemple.com"
+          />
+
+          <label for="email" class="form-label mt-3">
+            Veuillez confirmer votre email
+          </label>
+          <input
+            type="text"
+            class="form-control"
+            placeholder="EX: exemple@exemple.com"
+          />
+
+          <label for="mot de passe" class="form-label mt-2">
+            Votre mot de passe
+          </label>
+          <input type="mdp" class="form-control" place-holder="****" />
+
+          <label for="mot de passe" class="form-label mt-2">
+            Veuillez confirmer votre mot de passe
+          </label>
+          <input
+            type="mot de passe"
+            class="form-control"
+            place-holder="*****"
+          />
+
+          <div class="text-end mt-2">
+            <button class="btn btn-outline-primary border-light shadow-lg rounded">
+              S'inscrire !
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default SignUp;
